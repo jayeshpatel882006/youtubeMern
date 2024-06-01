8782ff32bd432dcf9a9bf727102da8c4c9ed3fd6
jayeshpatel882006 <jayu07498@gmail.com>
2024-06-01 17:55:46 +0530

    Added the route

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,12 @@ app.use(express.static("public"))
 app.use(cookaiParser())
 
 
+//Router Starrts From hear
+//import all Route
+import UserRouter from "./routes/user.routes.js"
+
+//user Of Route
+app.use("/api/v1/users",UserRouter)
 
 
 export {app}
